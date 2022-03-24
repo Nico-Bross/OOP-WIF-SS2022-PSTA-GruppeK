595f826d04115f9f01bb1beadd8184715876c5a0
Michael Wenisch <Gruppe_C>
2022-03-24 14:56:32 +0100

    Employee Konstruktor + Getter/Setter + Main Korrigiert

--- src/TheGoldenBucket/Employee.java
+++ src/TheGoldenBucket/Employee.java
@@ -1,6 +1,23 @@
 package TheGoldenBucket;
 
 public class Employee {
-    String name;
-    String title;
+    private String name;
+    private String title;
+
+    public Employee (String name, String title){
+        this.name=name;
+        this.title=title;}
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){ return name;}
+
+    public void setTitle(String title){this.title=title;}
+
+    public String getTitle() {
+        return title;
+    }
 }
+
+
