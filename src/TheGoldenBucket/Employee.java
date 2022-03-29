b6be195c1ca54c95647aa550d0fd877caa7c9c41
Melina <melina.vandrey@stud.th-rosenheim.de>
2022-03-29 20:36:42 +0200

    getter und setter Methoden vervollständigt

--- src/TheGoldenBucket/Employee.java
+++ src/TheGoldenBucket/Employee.java
@@ -9,4 +9,20 @@ public class Employee {
         this.name = name;
         this.title = title;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- src/TheGoldenBucket/Food.java
+++ src/TheGoldenBucket/Food.java
@@ -9,4 +9,20 @@ public class Food {
         this.name = name;
         this.price = price;
     }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
