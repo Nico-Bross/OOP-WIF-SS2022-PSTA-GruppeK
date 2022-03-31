c3f03c4971242c60153686117b8e8ac8dc3bb154
Michael Wenisch <Gruppe_C>
2022-03-31 16:39:16 +0200

    Sichtbarkeiten

--- src/TheGoldenBucket2/ListFoods.java
+++ src/TheGoldenBucket2/ListFoods.java
@@ -1,9 +1,9 @@
 package TheGoldenBucket2;
 
 public interface ListFoods {
-    public boolean add (Food f);
-    public boolean contains (Food f);
-    public boolean remove (Food f);
-    public int size ();
-    public ListFoodsImpl.ListElement getFirst ();
+     boolean add (Food f);
+     boolean contains (Food f);
+     boolean remove (Food f);
+     int size ();
+     ListFoodsImpl.ListElement getFirst ();
 }
