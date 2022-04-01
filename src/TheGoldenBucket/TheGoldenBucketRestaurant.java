970b26e2bd6acceac85794a89ce9097e5885fe6f
Johannes Beyer <j.beyer@beyerinformatik.de>
2022-04-01 12:37:22 +0200

    combined all changes in the main

--- src/TheGoldenBucket/TheGoldenBucketRestaurant.java
+++ src/TheGoldenBucket/TheGoldenBucketRestaurant.java
@@ -5,44 +5,44 @@ public class TheGoldenBucketRestaurant {
         Customer max = new Customer();
         max.name="Maxwell Powers";
 
-        Reservation maxReservation = new Reservation();
-        maxReservation.c=max;
-        maxReservation.date="31.03.2022";
-        maxReservation.time="8pm";
+        Reservation maxReservation = new Reservation(max,"31.03.2022","8pm");
 
         Employee peter = new Employee();
-        peter.name="Peter Griffin";
-        peter.title="Waiter";
+        peter.setEmployee("Peter", "Waiter");
 
         Employee lois = new Employee();
-        lois.title="Cook";
-        lois.name="Lois Griffin";
+        lois.setEmployee("Lois", "Cook");
+
 
         maxReservation.waiter=peter;
 
         // Later that evening Maxwell Powers likes to order some food and drinks
-        Order o = new Order();
-        o.drinks=new Drink[2];
-        o.drinks[0]=new Drink();
-        o.drinks[0].name="Coke";
-        o.drinks[0].price=395;
-        o.drinks[1]=new Drink();
-        o.drinks[1].name="Negroni Cocktail";
 
-        o.addFood(new Food());
-        o.addFood(new Food());
-        o.addFood(new Food());
-        o.foods[0].name="Pizza Magherita";
-        o.foods[1].name="Antipasti Selection";
-        o.foods[0].price=1050;
-        o.foods[1].price=970;
+
+
+        Order o = new Order();
+        o.addDrink(new Drink("Coke", 395));
+        o.addDrink(new Drink("Negroni Cocktail", 0));
+
+        o.addFood(new Food("Pizza Magherita", 1050));
+        o.addFood(new Food("Antipasti Selection", 970));
+
+        float gesamt = 0;
+
+        for (int i = 0; i<=o.number_of_drinks-1; i++){
+            gesamt = gesamt +  o.drinks[i].getPrice();
+        }
+        for (int j = 0; j<=o.number_of_foods-1; j++){
+            gesamt = gesamt + o.foods[j].getPrice();
+        }
+
 
         System.out.println( "Dear Guest "+
-                            max.name+
-                            " We thank you so much for yur Business. Tonight you had "+
-                            o.number_of_drinks+
-                            " Drinks and you ordered "+
-                            o.number_of_foods+
-                            " different variations of our food. ");
+                max.name+
+                " we thank you so much for your Business. Tonight you had "+
+                o.number_of_drinks+
+                " Drinks" + '\n' +o.drinks[0].getName() + ", " +  o.drinks[1].getName() + '\n' + "and you ordered "+
+                o.number_of_foods+
+                " different variations of our food" + '\n' +o.foods[0].getName() + ", " +  o.foods[1].getName() + '\n' + "Total: " + gesamt + " €");
     }
 }
