7122d8bfdc92d339f813df222d2986d66b8d13ad
studbrosle3454 <leonnicolas.bross@stud.th-rosenheim.de>
2022-04-01 12:34:22 +0200

    Employee + Food

--- src/TheGoldenBucket/Employee.java
+++ src/TheGoldenBucket/Employee.java
@@ -3,4 +3,26 @@ package TheGoldenBucket;
 public class Employee {
     String name;
     String title;
+
+    public Employee(){}
+
+    private Employee(String EmployeeName, String EmployeeTitle){
+        this.name=EmployeeName;
+        this.title=EmployeeTitle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setEmployee(String name, String title) {
+        this.name = name;
+        this.title = title;
+    }
+
+
 }
--- src/TheGoldenBucket/Food.java
+++ src/TheGoldenBucket/Food.java
@@ -2,5 +2,26 @@ package TheGoldenBucket;
 
 public class Food {
     String name;
-    int price;
+    float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public Food(){}
+
+    public Food(String name, float price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setFood(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
 }
