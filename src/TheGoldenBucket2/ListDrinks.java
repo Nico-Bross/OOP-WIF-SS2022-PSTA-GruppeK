bba4c901005f111f1fcd6a66be13e54bc3efefa7
stugoetsi5303 <simon.goettsberger@stud.th-rosenheim.de>
2022-04-08 13:16:59 +0200

    Branch bereinigt

--- src/TheGoldenBucket2/ListDrinks.java
+++
@@ -1,9 +0,0 @@
-package TheGoldenBucket2;
-
-public interface ListDrinks {
-    public boolean add (Drink d);
-    public boolean contains (Drink d);
-    public boolean remove (Drink d);
-    public int size ();
-    public ListDrinksImpl.ListElement getFirst ();
-}
--- src/TheGoldenBucket2/ListFoods.java
+++
@@ -1,9 +0,0 @@
-package TheGoldenBucket2;
-
-public interface ListFoods {
-    public boolean add (Food f);
-    public boolean contains (Food f);
-    public boolean remove (Food f);
-    public int size ();
-    public ListFoodsImpl.ListElement getFirst ();
-}
