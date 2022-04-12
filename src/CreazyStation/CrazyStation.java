4c0c8df6b546c0e7f7aea78e4a41265735eaf0da
studstelch2896 <christoph.stellner@stud.th-rosenheim.de>
2022-04-12 18:59:38 +0200

    a

--- src/CreazyStation/CrazyStation.java
+++ src/CreazyStation/CrazyStation.java
@@ -12,13 +12,13 @@ public class CrazyStation {
 
         CentralStation frankfurt = new CentralStation("Frankfurt");
 
-        Train munich_frankfurt = new Train(munich, frankfurt);
-        Train hamburg_frankfurt = new Train(hamburg, frankfurt);
-        Train berlin_frankfurt = new Train(berlin, frankfurt);
-        Train cologne_frankfurt = new Train(cologne, frankfurt);
+        Train[] zug =new Train[]{,
+        new Train(berlin, frankfurt),
+        new Train(hamburg, frankfurt),
+        new Train(cologne, frankfurt)};
 
-        hamburg.addTrain(hamburg_frankfurt);
-        munich.addTrain(munich_frankfurt);
+        hamburg.addTrain(zug[]);
+        munich.addTrain(zug[;
         cologne.addTrain(cologne_frankfurt);
         berlin.addTrain(berlin_frankfurt);
         frankfurt.addTrain(hamburg_frankfurt);
@@ -27,36 +27,32 @@ public class CrazyStation {
         frankfurt.addTrain(berlin_frankfurt);
 
 
-        Car c1 = new Car(1, hamburg, cologne);
-        Car c2 = new Car(2, hamburg, cologne);
-        Car c3 = new Car(3, hamburg, munich);
-        Car c4 = new Car(4, hamburg, munich);
-        Car c5 = new Car(5, munich, cologne);
-        Car c6 = new Car(6, berlin, munich);
-        Car c7 = new Car(7, berlin, hamburg);
-        Car c8 = new Car(8, cologne, munich);
-        Car c9 = new Car(9, munich, cologne);
-        Car c10 = new Car(10, munich, berlin);
-        Car c11 = new Car(11, cologne, berlin);
-        Car c12 = new Car(12, berlin, munich);
-        Car c13 = new Car(13, berlin, hamburg);
-        Car c14 = new Car(14, berlin, munich);
+        Car[] cars =new Car[]{new Car(1, hamburg, cologne),
+        new Car(2, hamburg, cologne),
+        new Car(3, hamburg, munich),
+        new Car(4, hamburg, munich),
+        new Car(5, munich, cologne),
+        new Car(6, berlin, munich),
+        new Car(7, berlin, hamburg),
+        new Car(8, cologne, munich),
+        new Car(9, munich, cologne),
+        new Car(10, munich, berlin),
+        new Car(11, cologne, berlin),
+        new Car(12, berlin, munich),
+        new Car(13, berlin, hamburg),
+        new Car(14, berlin, munich)};
 
 
-        hamburg.addCar(c1);
-        hamburg.addCar(c2);
-        hamburg.addCar(c3);
-        hamburg.addCar(c4);
-        munich.addCar(c5);
-        berlin.addCar(c6);
-        berlin.addCar(c7);
-        cologne.addCar(c8);
-        munich.addCar(c9);
-        munich.addCar(c10);
-        cologne.addCar(c11);
-        berlin.addCar(c12);
-        berlin.addCar(c13);
-        berlin.addCar(c14);
+       for (Car c:cars)
+       {if (c.getStart()==hamburg)
+            hamburg.addCar(c);
+          else if (c.getStart()==munich)
+        munich.addCar(c);
+          else if (c.getStart()==berlin)
+        berlin.addCar(c);
+          else if (c.getStart()==cologne)
+        cologne.addCar(c);
+       }
 
         System.out.println("__________________Situation at the beginning of the day__________________");
         System.out.println(hamburg);
