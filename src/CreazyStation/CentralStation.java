284dd6278c0d0bd94510faf5477a63febd5c5a97
Johannes Beyer <j.beyer@beyerinformatik.de>
2022-04-24 11:06:13 +0200

    CentralStation

--- src/CreazyStation/CentralStation.java
+++ src/CreazyStation/CentralStation.java
@@ -2,142 +2,134 @@ package CreazyStation;
 
 public class CentralStation{
 
+    private LinkedList storage;
+    private Tree trains;
+    private String name;
+
     public CentralStation(String name){
         this.name = name;
-        storage = new Car[1];
-        trains = new Train[1];
+        storage = new LinkedList();
+        trains = new Tree();
     }
 
     public CentralStation(String name, Train train){
         this.name = name;
-        storage = new Car[1];
-        trains = new Train[1];
-        trains [1] = train;
+        storage = new LinkedList();
+        trains = new Tree();
+        trains.insert(train);
     }
 
-    public CentralStation(String name, Train[] trains){
+    public CentralStation(String name, Tree trains){
         this.name = name;
-        storage = new Car[1];
+        storage = new LinkedList();
         this.trains = trains;
     }
 
-    private Car[] storage;
-    private Train[] trains;
-    private String name;
-
-    public void addTrain (Train c) {
-        if (trains[0] == null) {
-            trains[0] = c;
-        } else {
-            Train[] newArray = new Train[trains.length + 1];
-            for (int i = 0; i < trains.length; i++) {
-                newArray[i] = trains[i];
-            }
-            newArray[trains.length] = c;
-            trains = newArray;
-        }
+    public void addTrain (Train train) {
+        trains.insert(train);
     }
 
-    public boolean addCar (Car c){
-        if (storage[0] == null){
-            storage [0] = c;
-            return true;
-        } else {
-            Car[] newArray = new Car[storage.length+1];
-            for (int i = 0; i < storage.length; i++ ){
-                newArray[i] = storage[i];
-            }
-            newArray [storage.length] = c;
-            storage = newArray;
-            return true;
-        }
+    public void addCar (Car c){
+        storage.insertLast(c);
     }
 
     public Car removeCar (){
-        if (storage.length == 0){
-            return null;
-        }
-        Car car = storage[storage.length-1];
-        Car[] newArray = new Car[storage.length-1];
-        for (int i = 0; i < storage.length-1; i++ ){
-            newArray[i] = storage[i];
-        }
-        storage = newArray;
-        return car;
+        return storage.deleteFirst();
     }
 
     public void loadTrains (){
-        Car c = removeCar();
-        while (c != null){
-            for (Train t: trains) {
-                while (c != null) {
-                    if (t.addCar(c)) {
-                        c = removeCar();
-                    } else {
-                        break;
-                    }
-                }
+        Car currentCar = storage.deleteFirst();
+        Train tr = trains.root.train;
+        while (currentCar != null) {
+            String destination = currentCar.getDestination().getName();
+            if(tr.getStation().getName().compareTo(destination) != 0){
+                getDestinationTrain(trains.root, currentCar);
+            } else {
+                tr.addCar(currentCar);
             }
-            c = removeCar();
+            currentCar = storage.deleteFirst();
         }
-        storage = new Car[1];
+//        if (storage.head != null){
+//            ListElement current = storage.head;
+//            while(current != null){
+//                Train tr = trains.root.train;
+//                String destination = current.car.getDestination().getName();
+//                if(tr.getStation().getName().compareTo(destination) != 0){
+//                    getDestinationTrain(trains.root, current.car);
+//                }else{
+//                    tr.addCar(current.car);
+//                }
+//                current = current.next;
+//            }
+//        }
     }
 
-    public void unloadTrains () {
-
-        for (Train t: trains){
-            Car c = t.removeCar();
-            while (c!=null){
-                addCar(c);
-                c = t.removeCar();
+    public void getDestinationTrain(Node parent, Car c){
+        if(parent.left != null){
+            if(parent.left.train.getStation().getName().compareTo(c.getDestination().getName()) != 0){
+                getDestinationTrain(parent.left, c);
+            }else{
+                parent.left.train.addCar(c);
             }
+        }
+        if(parent.right != null){
+            if(parent.right.train.getStation().getName().compareTo(c.getDestination().getName()) != 0){
+                getDestinationTrain(parent.right, c);
+            }else{
+                parent.right.train.addCar(c);
+            }
+        }
+    }
+
+    public void unloadTrains() {
+        if (trains.root != null){
+            unloadTrain(trains.root.train);
+            unloadTrains(trains.root);
+        }
+    }
+
+    public void unloadTrains(Node parent){
+        if(parent.left != null){
+            unloadTrain(parent.left.train);
+            unloadTrains(parent.left);
+        }
+        if(parent.right != null){
+            unloadTrain(parent.right.train);
+            unloadTrains(parent.right);
+        }
+    }
+
+    public void unloadTrain (Train t) {
+        Car c = t.removeCar();
+        while (c != null){
+            addCar(c);
+            c = t.removeCar();
         }
     }
 
     public String toString () {
         String s;
-        s = name + ":\n";
-        for (Car c: storage){
-            s += c.toString() + "\n";
-        }
+        s = "Central station =" + name + ":\n" + "Carstorage = " + storage.toString();
         return s;
     }
 
-    public Car[] getStorage (){return storage; }
+    public String toStringDetailed () {
+        String s;
+        s = "Central station =" + name + ":\n";
+        s += "car storage ="  + ":\n" + storage.toString();
+        s += "trains =" + ":\n" + trains.toString();
+        return s;
 
-    public void setStorage (Car[] sorted){
+    }
+
+    public LinkedList getStorage (){return storage; }
+
+    public void setStorage (LinkedList sorted){
         storage = sorted;
     }
 
-    public Train[] getTrains ()  { return trains; }
+    public Tree getTrains ()  { return trains; }
 
     public String getName() { return name; }
 
-    public void distributeCars (){
-        Car[] notDistributable = new Car[1];
-        while (storage.length != 0){
-            Car c = removeCar();
-            for (Train t: getTrains()){
-                if (t.getStation().getName() == c.getTarget().getName()){
-                    if (t.addCar(c)){
-                        break;
-                    } else {
-                        if (c != null){
-                            if (notDistributable[0] == null) {
-                                notDistributable[0] = c;
-                            } else {
-                                Car[] newArray = new Car[notDistributable.length + 1];
-                                for (int i = 0; i < notDistributable.length; i++) {
-                                    newArray[i] = notDistributable[i];
-                                }
-                                newArray[notDistributable.length] = c;
-                                notDistributable = newArray;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        storage = new Car[1];
-    }
 }
